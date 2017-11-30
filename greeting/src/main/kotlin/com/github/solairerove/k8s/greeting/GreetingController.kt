7de5dce76303d63831436730e8b8a121acb3eafd
solairerove <hax2033@gmail.com>
2017-12-01 01:21:34 +0300

    feat: create simple spring cloud config application - update docker files - update docker compose file - add spring boot starter web - update pom files

--- greeting/src/main/kotlin/com/github/solairerove/k8s/greeting/GreetingController.kt
+++ greeting/src/main/kotlin/com/github/solairerove/k8s/greeting/GreetingController.kt
@@ -1,5 +1,6 @@
 package com.github.solairerove.k8s.greeting
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -11,9 +12,12 @@ import java.util.concurrent.atomic.AtomicLong
 @RestController
 class GreetingController {
 
+    @Value("\${profile}")
+    var profile: String? = null
+
     private val counter = AtomicLong()
 
     @GetMapping("/greeting")
     fun greeting(@RequestParam("name", defaultValue = "World") name: String) =
-            Greeting(counter.incrementAndGet(), "Hello, $name")
+            Greeting(counter.incrementAndGet(), "Hello, $name. Ur profile is $profile")
 }
