dcb49f1a89c3e7387e1696604c033ba7fe5cbac8
solairerove <hax2033@gmail.com>
2017-12-01 14:57:01 +0300

    feat: bootstrap eureka server

--- greeting/src/main/kotlin/com/github/solairerove/k8s/greeting/GreetingApplication.kt
+++ greeting/src/main/kotlin/com/github/solairerove/k8s/greeting/GreetingApplication.kt
@@ -2,8 +2,10 @@ package com.github.solairerove.k8s.greeting
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class GreetingApplication
 
 fun main(args: Array<String>) {
