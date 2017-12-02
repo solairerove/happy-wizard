a25f079e4a3535415068a19401617c6e6e083c25
solairerove <hax2033@gmail.com>
2017-12-03 02:35:31 +0300

    feat: fetch data from task service due load balancer

--- greeting/src/main/kotlin/com/github/solairerove/k8s/greeting/GreetingController.kt
+++ greeting/src/main/kotlin/com/github/solairerove/k8s/greeting/GreetingController.kt
@@ -1,16 +1,18 @@
 package com.github.solairerove.k8s.greeting
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.RestTemplate
 import java.util.concurrent.atomic.AtomicLong
 
 /**
  * Greeting rest controller.
  */
 @RestController
-class GreetingController {
+class GreetingController(@Autowired val restTemplate: RestTemplate) {
 
     @Value("\${profile}")
     var profile: String? = null
@@ -18,6 +20,10 @@ class GreetingController {
     private val counter = AtomicLong()
 
     @GetMapping("/greeting")
-    fun greeting(@RequestParam("name", defaultValue = "World") name: String) =
+    fun greeting(@RequestParam("name", defaultValue = "World") name: String): Greeting? =
             Greeting(counter.incrementAndGet(), "Hello, $name. Ur profile is $profile")
+
+    @GetMapping("/tasks")
+    fun tasks(@RequestParam("service", defaultValue = "task") service: String): String =
+            restTemplate.getForObject("http://$service/tasks", String::class.java)
 }
